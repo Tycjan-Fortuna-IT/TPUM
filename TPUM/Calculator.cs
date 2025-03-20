32151459f6e04a6753d9b8dccd5d5478bdd1eed9
Tycjan Fortuna <242213@edu.p.lodz.pl>
2025-03-20 19:46:00 +0100

    Task 0

---
+++ TPUM/Calculator.cs
@@ -0,0 +1,27 @@
+﻿namespace TPUM;
+
+public class Calculator
+{
+    public int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    public int Subtract(int x, int y)
+    {
+        return x - y;
+    }
+
+    public int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+
+    public int Divide(int x, int y)
+    {
+        if (y != 0)
+            return x / y;
+
+        throw new ArgumentException("You cannot divide by zero!");
+    }
+}
